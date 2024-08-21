799abac0a0ad39970913d7b271eca9adf006ab59
shivam <shivamsharma3172004@gmail.com>
2024-08-21 16:07:54 +0530

    ..

--- client/src/components/Cube.jsx
+++ client/src/components/Cube.jsx
@@ -2,16 +2,46 @@ import { useBox } from "@react-three/cannon";
 import React from "react";
 import * as textures from "../images/textures.jsx";
 import { NearestFilter, RepeatWrapping } from "three";
+import useStore from "../hooks/useStore.jsx";
 const Cube = ({ position, texture }) => {
   const [reference] = useBox(() => ({
     type: "Static",
     position,
   }));
   const activeTexture = textures[texture + "Texture"];
+  const [addCube, removeCube] = useStore((state) => [
+    state.addCube,
+    state.removeCube,
+  ]);
 
-  console.log(texture);
   return (
-    <mesh ref={reference}>
+    <mesh
+      onClick={(e) => {
+        e.stopPropagation();
+        const faceIndex = Math.floor(e.faceIndex / 2);
+        const { x, y, z } = reference.current.position;
+        if (faceIndex === 0) {
+          addCube(x + 1, y, z);
+          return;
+        } else if (faceIndex === 1) {
+          addCube(x - 1, y, z);
+          return;
+        } else if (faceIndex === 2) {
+          addCube(x, y + 1, z);
+          return;
+        } else if (faceIndex === 3) {
+          addCube(x, y - 1, z);
+          return;
+        } else if (faceIndex === 4) {
+          addCube(x, y, z + 1);
+          return;
+        } else if (faceIndex === 5) {
+          addCube(x, y, z - 1);
+          return;
+        }
+      }}
+      ref={reference}
+    >
       <boxGeometry attach="geometry" />
       <meshStandardMaterial attach="material" map={activeTexture} />
     </mesh>
